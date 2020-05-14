006b1b65502d146b7ff29e2892c63999c4d3a6b3
randa yahya <randaalrayan96@gmial.com>
2020-05-14 15:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution here!
+append = ["Milo", "Otis", "Garfield"];
+prepend = ["Milo", "Otis", "Garfield"];
+removeLast  = ["Milo", "Otis", "Garfield"];
+removeFirst = ["Milo", "Otis", "Garfield"];
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
